c1d3da28ec377644473ca18f23db9198e4a12fa9
Sarat1902 <161099567+Sarat1902@users.noreply.github.com>
2024-04-17 21:03:26 +0000

    commit

--- app/controllers/math_controller.rb
+++ app/controllers/math_controller.rb
@@ -23,6 +23,33 @@ class MathController < ApplicationController
     render ({ :template => "math_templates/square_root_result"})
   end
 
+  def payment
+    render ({ :template => "math_templates/payment"})
+  end
+
+  def payment_result
+   @interest = params.fetch("user_apr").to_f 
+   @rate = @interest.to_fs(:percentage,{:precision => 4})
+
+   @r = @interest / 12 / 100
+
+   @years = params.fetch("user_years").to_i
+   @n = @years * 12
+
+   @principal = params.fetch("user_pv").to_f
+   @p = @principal.to_fs(:currency)
+  
+   @numerator = @principal * @r
+  
+   @d =  1 + @r
+   @db = @d ** @n
+   @dr = 1 / @db
+   @denom = 1 - @dr
+  
+   @payment = @numerator/@denom
+   @results = @payment.to_fs(:currency, {:precision => 2})
+   render ({ :template => "math_templates/payment_result"})
+  end
 
 
 
