67ebc839ca74406a1ad0e848cb5d6a80b603e315
Sarat1902 <161099567+Sarat1902@users.noreply.github.com>
2024-04-17 20:48:20 +0000

    commit

--- app/controllers/math_controller.rb
+++ app/controllers/math_controller.rb
@@ -7,5 +7,23 @@ class MathController < ApplicationController
     render ({ :template => "math_templates/square"})
   end
 
+  def square_result
+    @num = params.fetch("number").to_f
+    @result = @num ** 2
+    render ({ :template => "math_templates/square_result"})
+  end
+
+  def square_root
+    render ({ :template => "math_templates/square_root"})
+  end
+
+  def square_root_result
+    @num = params.fetch("user_number").to_f
+    @result = @num ** 0.5
+    render ({ :template => "math_templates/square_root_result"})
+  end
+
+
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,8 @@ Rails.application.routes.draw do
   get("/square_root/new", { :controller => "math", :action => "square_root" })
   get("/payment/new", { :controller => "math", :action => "payment" })
   get("/random/new", { :controller => "math", :action => "random" })
-
+  get("/square/results", { :controller => "math", :action => "square_result" })
+  get("/square_root/results", { :controller => "math", :action => "square_root_result" })
+  get("/payment/results", { :controller => "math", :action => "payment_result" })
+  get("/random/results", { :controller => "math", :action => "random_result" })
 end
