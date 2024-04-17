d0856a4d103ade4d82ac941079ea107b2848ae05
Sarat1902 <161099567+Sarat1902@users.noreply.github.com>
2024-04-17 20:30:11 +0000

    commit

--- app/controllers/math_controller.rb
+++ app/controllers/math_controller.rb
@@ -1,7 +1,11 @@
-class MathController > ApplicationController
+class MathController < ApplicationController
+  def show
+    render ({ :template => "math_templates/home"})
+  end
 
- def show
-   render ({ :template => "math_templates/home"})
- end
+  def square
+    render ({ :template => "math_templates/square"})
+  end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   
-  get ("/" , {:controller => "math", :action => "show"})
-
-
-
-
+  get("/", { :controller => "math", :action => "show" })
+  get("/square/new", { :controller => "math", :action => "square" })
+  get("/square_root/new", { :controller => "math", :action => "square_root" })
+  get("/payment/new", { :controller => "math", :action => "payment" })
+  get("/random/new", { :controller => "math", :action => "random" })
 
 end
